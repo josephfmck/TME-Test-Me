0eccf6898b2debb9aae80b785c2dfa8ca15e56b6
Joe <josephfmck@gmail.com>
2020-08-26 15:57:13 -0500

    create runner.js, setup implementation steps

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 #!/usr/bin/env node
 
-//*In terminal
-//npm init -y   creates package.json
-//npm link      creates package.lock
-
-//*In package.json 
 //*to run in terminal:  tme
-// "bin": {
-//     "tme": "index.js"
-//   }
-//
-//*ONLY ABLE TO RUN WITH tme IF ITS THIS DIRECTORY
+
 
 console.log("Running test!!...");
---
+++ runner.js
@@ -0,0 +1,11 @@
+//*contains code relating to process of: collecting all files, calling function with environment setup, and run each test file
+
+
+//*finds all files in ".test.js" recursively through a folder
+//*Store a reference to each file we find
+//*After getting a full list of the test files, execute them one by one
+class Runner {
+
+}
+
+module.exports = Runner;
