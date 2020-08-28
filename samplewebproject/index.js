236a98bd1b92ab61f40d228067a2a33b3c6e47d1
Joe <josephfmck@gmail.com>
2020-08-28 14:33:08 -0500

    474 Sample Web App, install jsdom dependency, create samplewebproject to start testing jsdom on

---
+++ samplewebproject/index.js
@@ -0,0 +1,15 @@
+document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const { value } = document.querySelector("input");
+
+    const header = document.querySelector("h1");
+
+    if(value.includes("@")) {
+        // valid
+        header.innerHTML = "looks good";
+    } else {
+        //invalid
+        header.innerHTML = "invalid email";
+    }
+});
