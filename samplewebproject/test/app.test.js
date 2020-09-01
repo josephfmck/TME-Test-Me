b0271c11c305195fa5886f30ac2006bebfd3a6f1
Joe <josephfmck@gmail.com>
2020-09-01 14:45:32 -0500

    479 Another Small Issue, create test to show message with valid email, simulating a form submission and logging the contents of h1

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -1,6 +1,5 @@
-const { italic } = require("chalk");
-
 const assert = require("assert");
+const render = require("../../render");
 
 it("has a text input", async () => {
     const dom = await render("index.html");
@@ -8,4 +7,22 @@ it("has a text input", async () => {
     const input = dom.window.document.querySelector("input");
 
     assert(input); //throws err if not found
+});
+
+it("shows a success message with a valid email", async () => {
+    //render dom
+    const dom = await render("index.html");
+
+    //changing input
+    const input = dom.window.document.querySelector("input");
+    input.value = "saajk@sf.com";
+
+    //select form and simulate submitting form event
+    const form = dom.window.document
+    .querySelector("form")
+    .dispatchEvent(new dom.window.Event("submit"));
+
+    const h1 = dom.window.document.querySelector("h1");
+
+    console.log("contents of h1", h1.innerHTML);
 });
